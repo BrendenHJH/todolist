59f3096f1f8d0a3eb697433e71b41bd7f99d736a
user-PC\user <brenden0730@naver.com>
2017-06-03 18:03:21 +0900

    TodoControllerTest 내용 추가

--- server/src/test/java/kr/or/connect/todo/api/TodoControllerTest.java
+++ server/src/test/java/kr/or/connect/todo/api/TodoControllerTest.java
@@ -1,6 +1,8 @@
 package kr.or.connect.todo.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -10,18 +12,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
-import kr.or.connect.todo.TodoApplication;
-
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = TodoApplication.class)
-@WebAppConfiguration
+@SpringBootTest
+@Transactional
 public class TodoControllerTest {
 	
 	@Autowired
@@ -48,5 +48,25 @@ public class TodoControllerTest {
 			.andExpect(jsonPath("$.id").exists())
 			.andExpect(jsonPath("$.todo").value("hello world6"))
 			.andExpect(jsonPath("$.completed").value(0));
+	}
+	@Test
+	public void shouldUpdate() throws Exception {
+		String requestBody = "{\"todo\":\"hello world6\", \"completed\":\"1\"}";
+
+		mvc.perform(
+			put("/api/todos/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody)
+			)
+			.andExpect(status().isNoContent());
+	}
+
+	@Test
+	public void shouldDelete() throws Exception {
+		mvc.perform(
+			delete("/api/books")
+				.contentType(MediaType.APPLICATION_JSON)
+		)
+		.andExpect(status().isNoContent());
 	}
 }
